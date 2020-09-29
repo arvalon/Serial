1f9a360ce57570483f22a9b6196a0fdeaaf70d31
Vaganov <vaganov@scancode.ru>
2020-09-29 16:08:43 +0300

    Удаление всех лишних вещей кроме получения SN штатным образом; Перевод комментариев с русского на английский.

--- app/src/main/java/ru/scancode/serial/MainActivity.java
+++ app/src/main/java/ru/scancode/serial/MainActivity.java
@@ -11,22 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import org.apache.commons.lang3.StringUtils;
-import org.xml.sax.SAXException;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-import ru.scancode.funlib.FunUser;
-import ru.scancode.testlib.StubFoo;
-import ru.scancode.testlib2.StubBar;
-
 public class MainActivity extends AppCompatActivity {
 
 	private static final CharSequence LOW_SDK = "Low SDK";
@@ -57,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 			try {
 				serial_new_tv.append(Build.getSerial());
 			} catch (SecurityException e) {
-				Logs.info(this,"Ошибка при получении SN: "+e.getMessage());
+				Logs.info(this,"Get serial error: "+e.getMessage());
 				e.printStackTrace();
 			}
 		}else {
@@ -66,100 +50,6 @@ public class MainActivity extends AppCompatActivity {
 
 		Logs.info(this,"Software version: "+Build.FINGERPRINT);
 
-		// package org.xmlpull.v1
-
-		XmlPullParserFactory factory = null;
-
-		try {
-
-			factory = XmlPullParserFactory.newInstance();
-			factory.setNamespaceAware(true);
-			XmlPullParser xpp = null;
-			xpp = factory.newPullParser();
-
-		} catch (XmlPullParserException e) {
-			e.printStackTrace();
-		}
-
-		// package javax.xml
-
-		DocumentBuilderFactory factory2 = DocumentBuilderFactory.newInstance();
-		factory2.setNamespaceAware(true);
-
-		try {
-
-			DocumentBuilder builder = factory2.newDocumentBuilder();
-
-			//Document doc = builder.parse(new File("path"));
-
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}
-
-		// SAX parser
-
-		SAXParserFactory factory3 = SAXParserFactory.newInstance();
-		try {
-
-			SAXParser parser = factory3.newSAXParser();
-			//parser.parse(new File("path"),new DefaultHandler());
-
-		} catch (ParserConfigurationException | SAXException e) {
-			e.printStackTrace();
-		}
-
-		char divChar = 0x01;
-
-		String divString = String.valueOf(divChar);
-
-		//Logs.info(this,"NumericValue: "+Character.getNumericValue(divChar)+", char: "+divChar+", String: "+divString);
-
-		// Тестирование подключённого модуля
-
-        // Из модуля testlib
-		StubFoo stubFoo = new StubFoo(1,"Mike");
-		Logs.info(this,"StubFoo: "+stubFoo);
-
-        // Из модуля testlib2
-		StubBar stubBar = new StubBar(2,"John");
-		Logs.info(this,"StubBar: "+stubBar);
-
-		// Из AAR
-		FunUser user = new FunUser(3,"Willy");
-		Logs.info(this, "From AAR: "+user);
-
-		// Apache String Utils
-
-		String str1 = "abc";
-		String str2 = "xxx";
-
-		boolean a = StringUtils.containsAny(str1, str2,"abc");
-		boolean b = StringUtils.containsAny(str1, str2,"zzz");
-		boolean c = StringUtils.equalsAnyIgnoreCase(str1, str2,"abc");
-		boolean d = StringUtils.equalsAnyIgnoreCase(str1, str2,"ab");
-
-		Logs.info(this,"Apache StringUtils.containsAny, a: "+a+", b: "+b+", c: "+c+", d: "+d);
-
-		Logs.info(this,"Model: "+Build.MODEL);
-
-		stupidCasting();
-
-	}
-
-	private void stupidCasting() {
-
-		Logs.info(this,"byte <-> int transform");
-
-		byte[] testArray = new byte[]{1,2,3};
-
-		byte testNum1 = 1;
-		int testNum2 = 2;
-
-		boolean flag1 = testNum1 == testArray[0];
-		Logs.info(this, "flag 1 = "+flag1);
-
-		boolean flag2 = testNum2 == (int)testArray[1];
-		Logs.info(this, "flag 2 = "+flag2);
 	}
 
 	private void checkPermissions() {
